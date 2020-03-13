44aa1fb609a6a14b600c6668a54fd423cfcee58b
Ravindra-DevOps <thanishinfotech01@gmail.co>
2020-03-13 18:19:25 +0530

    4th commit

--- hellojava.java
+++ hellojava.java
@@ -2,13 +2,13 @@ class hellojava   //this is a class name of// the java program
 {
 public static void main(String[] args)  //its a main method 
 {
-int a,b,c; //variables
-a=500; //assigning values to variable
+int d,a,b,c; //variables
+a=500; 
 b=23;
 c=123;
-c=a+b+c;//calculation
+c=a+b+c;
 System.out.println("The sum of three values are "+c);//printing the output
-int d=c-a;
-System.out.println("The d value is "+d);
+d=c-a;
+System.out.println("The d  is "+d);
 }//closing the main method
-}//closing the class ofthe program
+}//closing the class ofthe
