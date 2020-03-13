f3875ad635123d57b0f68ca421c677851db4eb85
Ravindra-DevOps <59326637+Ravindra-DevOps@users.noreply.github.com>
2020-03-13 17:59:19 +0530

    Update hellojava.java

--- hellojava.java
+++ hellojava.java
@@ -1,12 +1,12 @@
-class hellojava
+class hellojava   //this is a class name of// the java program
 {
-public static void main(String[] args)
+public static void main(String[] args)  //its a main method 
 {
-int a,b,c;
-a=500;
+int a,b,c; //variables
+a=500; //assigning values to variable
 b=23;
 c=123;
-c=a+b+c;
-System.out.println("The sum of three values are "+c);
-}
-}
+c=a+b+c;//calculation
+System.out.println("The sum of three values are "+c);//printing the output
+}//closing the main method
+}//closing the class ofthe program
